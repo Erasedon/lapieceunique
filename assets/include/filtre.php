2d2734a555c59cbe3541d431df2c0ca79af71e95
yanis.pirlet <yanis.pirlet@gmail.com>
2022-08-28 15:59:54 +0200

    filtre update

--- assets/include/filtre.php
+++ assets/include/filtre.php
@@ -24,7 +24,7 @@ include 'assets/db/connectdb.php';
 
                             $query = "SELECT * FROM genres ORDER BY id_genres";
                             $statement = $db->prepare($query);
-                            $statement->execute();  
+                            $statement->execute();
                             $result = $statement->fetchAll();
                             foreach ($result as $row) {
                             ?>
@@ -96,181 +96,196 @@ include 'assets/db/connectdb.php';
                 </div>
             </div>
 
-            
-            
-            
-            
+
+
+
+
             <!-- RANGE PRIX -->
             <div class="card-body">
-                
-                
+
+
                 <div class="slidecontainer">
-                    
+
                     <h4>Prix</h4>
                     <input type="hidden" id="hidden_minimum_price" value="5" />
                     <input type="hidden" id="hidden_maximum_price" value="500" />
                     <p id="price_show">5 - 500</p>
                     <div id="price_range"></div>
                 </div>
-                
+
             </div>
-            
+
         </div>
         <div class="col-md-9">
             <br />
             <div class="row filter_data">
-                
-           
-                </div>
+
+
+            </div>
 
 
 
 
-        <!-- FIN filtre -->
+            <!-- FIN filtre -->
 
 
 
-        <script>
+            <script>
+                // Fonction qui va récupér les paramètres GET
 
-
-// Fonction qui va récupér les paramètres GET
-
-$(document).ready(function() {
-    var currentURL = document.URL;
-    function getParameterByName(name, url = window.location.href) {
-        name = name.replace(/[\[\]]/g, '\\$&'); // regex qui traite la chaine
-        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, ' '));
-    }
-    
-    // console.log('url: '+currentURL);
-    
-    var page = (getParameterByName('page') == null) ? 1 : getParameterByName('page');
-    var limit = (getParameterByName('limit') == null) ? 5 : getParameterByName('limit');
-    
-    filter_data(page, limit);
-    
-    function filter_data(page = 1, limit = 5) {
-                     
-        $('.filter_data').html('<div id="loading" style="" ></div>');
-        var action = 'fetch_data';
-        var minimum_price = $('#hidden_minimum_price').val();
-        var maximum_price = $('#hidden_maximum_price').val();
-        var brand = get_filter('brand');
-        
-        var ram = get_filter('ram');
-        var storage = get_filter('storage');
-        
-        
-        var  object = {
-                        // action: action,
-                        //     minimum_price: minimum_price,
-                        //     maximum_price: maximum_price,
-                        //     brand: brand,
-                        //     ram: ram,
-                        //     storage: storage,
-                        //     page: page
-                    }
-                    
-                    if (action=!null) {
-                        object.action=action;
-                    }
-                        if (limit!=null) {
-                            object.limit=limit;
-                            urlreplace = "limit="+limit+"&";
-                            console.log(urlreplace);
-                        }
-                            if (page!=null) {
-                                object.page = page;
-                                urlreplace += "page="+page+"&";
-                            }
-                                if (minimum_price!="") {
-                                    object.minimum_price = minimum_price;
-                                    urlreplace += "minimum_price="+minimum_price+"&";
-                                }
-                                    if (maximum_price!="") {
-                                        object.maximum_price = maximum_price;
-                                        urlreplace += "maximun_price="+maximum_price+"&";
-                                    }
-                                        if (brand!="") {
-                                            object.brand = brand;
-                                            urlreplace += "brand="+brand+"&";
-                                        }
-                                            if (ram!="") {
-                                                object.ram = ram;
-                                                urlreplace += "ram="+ram+"&";
-                                            }
-                                                if (storage!="") {
-                                                    object.storage = storage;
-                                                    urlreplace += "storage="+storage;
-                                                }
+                $(document).ready(function() {
                  
-                    
-                    $.ajax({
-                        url: "assets/include/cards.php",
-                        method: "GET",
-                        data: object,
-                        success: function(data) {
-                            $('.filter_data').html(data);
+                    var currentURL = document.URL;
 
-                            // urlresultat = window.location.replace('?'+urlreplace);   
+                    const url = new URL(window.location);
+
+                    // rechercher une valeur dans un url
+
+                    function getParameterByName(name, url = window.location.href) {
+                        name = name.replace(/[\[\]]/g, '\\$&'); // regex qui traite la chaine
+                        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                            results = regex.exec(url);
+                        if (!results) return null;
+                        if (!results[2]) return '';
+                        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+                    }
+
+
+                    var page = (getParameterByName('page') == null) ? 1 : getParameterByName('page');
+                    var limit = (getParameterByName('limit') == null) ? 5 : getParameterByName('limit');
+
+
+                    filter_data(page, limit);
+
+                    function filter_data(page = 1, limit = 5) {
+
+                        $('.filter_data').html('<div id="loading" style="" ></div>');
+                        var action = 'fetch_data';
+                        var minimum_price = $('#hidden_minimum_price').val();
+                        var maximum_price = $('#hidden_maximum_price').val();
+                        console.log(maximum_price, minimum_price);
+
+                        var brand = get_filter('brand');
+                        var ram = get_filter('ram');
+                        var storage = get_filter('storage');
+
+
+                        var object = {}
+
+                        if (action = !null) {
+                            object.action = action;
                         }
-                    });
+                        if (limit != null) {
+                            object.limit = limit;
+                        }
+                        if (page != null) {
+                            object.page = page;
+                        }
+                        if (minimum_price != "") {
+                            object.minimum_price = minimum_price;
+                        }
+                        if (maximum_price != "") {
+                            object.maximum_price = maximum_price;
+                        }
+                        if (brand != "") {
+                            object.brand = brand;
+                        }
+                        if (ram != "") {
+                            object.ram = ram;
+                        }
+                        if (storage != "") {
+                            object.storage = storage;
+                        }
 
-                }
-                
-                function get_filter(class_name) {
-                    var filter = [];
-                    $('.' + class_name + ':checked').each(function() {
-                        filter.push($(this).val());
-                    });
 
-                    return filter.toString();
-                }
+                        $.ajax({
+                            url: "assets/include/cards.php",
+                            method: "GET",
+                            data: object,
+                            success: function(data) {
+                                $('.filter_data').html(data);
+                                // console.log(data)
+
+                            }
+                        });
+
+                    }
+                    i=0;
+                    // recuperer la valeur des checkbox 
+                    function get_filter(class_name) {
+                        var filter = [];
                         
-                $('.common_selector').click(function() {
-                    filter_data();
-                    urlresultat = window.location.replace('?'+urlreplace);
-                });
-                
-             
-            $('.filter_data').on(
-                'click',
-                '.pag_selector',
-                function(e) {
-                    filter_data($(e.target).data('page'),limit);
-                    urlresultat = window.location.replace('?'+urlreplace);
-                });
-                
+                        $('.' + class_name + ':checked').each(function() {
+                            filter.push($(this).val()); 
+                            url.searchParams.set(class_name, filter);
+                            history.pushState({}, '', url);
+                        });
+                        
+                        $('.'+class_name).each(function() {
+                    
+                             console.log('test de false=');
+                            
+                            // console.log(this.checked === false);
+                            // url.searchParams.delete($(this));
+            
+
+                          
+                           history.pushState({}, '', url);
+                        });
+
+                        return filter.toString();
+
+                    }
 
 
-                $('.filter_data').on('click','.limit_selector', function(e) {                    
-                    filter_data(page,$(e.target).data('limit'));
-                    urlresultat = window.location.replace('?'+urlreplace);
-                });
-
-
-                $('#price_range').slider({
-                    range: true,
-                    min: 5,
-                    max: 500,
-                    values: [5, 500],
-                    step: 1,
-                    stop: function(event, ui) {
-                        $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
-                        $('#hidden_minimum_price').val(ui.values[0]);
-                        $('#hidden_maximum_price').val(ui.values[1]);
+                    $('.common_selector').on('click', function() {
                         filter_data();
+                    });
+
+
+                    $('.filter_data').on(
+                        'click',
+                        '.pag_selector',
+                        function(e) {
+                            const page = $(e.target).data('page');
+                            filter_data(page, limit);
+                            url.searchParams.set('page', page);
+                        history.pushState({}, '', url);
+                           
+                        });
+
+
+
+             
+                    $('.filter_data').on('click', '.limit_selector', function(e) {
+                        const limit = $(e.target).data('limit');
+                        filter_data(page, limit);
+
+                        url.searchParams.set('limit', limit);
+                        history.pushState({}, '', url);
+               
+                      
+                    });
+
+
+                    $('#price_range').slider({
+                        range: true,
+                        min: 5,
+                        max: 500,
+                        values: [5, 500],
+                        step: 1,
+                        stop: function(event, ui) {
+                            $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+                            $('#hidden_minimum_price').val(ui.values[0]);
+                            $('#hidden_maximum_price').val(ui.values[1]);
+                            filter_data();
+                       
                     }
                 });
-
+                
             });
-        </script>
 
-        <!-- FIN filtre -->
+            // urlresultat = history.pushState(currentURL,window.location.replace('?'+urlreplace.substr(0, urlreplace.length - 1)));  
+            </script>
 
-
-
+<!-- FIN filtre -->
